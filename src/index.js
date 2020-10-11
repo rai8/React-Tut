428d2df19bfa1512e0f8e196896776dab482fb67
rai8 <hytonne@outlook.com>
2020-10-11 19:17:22 +0300

    working with array map method

--- src/index.js
+++ src/index.js
@@ -3,42 +3,47 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 const root = document.getElementById('root')
-
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51HU5AkfapL._AC_SX184_.jpg',
-  author: 'Sandra Boynton ',
-  titleBook: 'Spooky Pookie (Little Pookie)',
-}
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._AC_SX184_.jpg',
-  author: 'Stephenie Meyer Stephenie Meyer  ',
-  titleBook: 'Midnight Sun ',
-}
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51HU5AkfapL._AC_SX184_.jpg',
+    author: 'Sandra Boynton ',
+    titleBook: 'Spooky Pookie (Little Pookie)',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._AC_SX184_.jpg',
+    author: 'Stephenie Meyer Stephenie Meyer  ',
+    titleBook: 'Midnight Sun ',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/417qEFl5D8L._SX437_BO1,204,203,200_.jpg',
+    author: 'Sandra Boynton  ',
+    titleBook: 'Eek! Halloween!  ',
+  },
+]
 
 const BookLIst = () => {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} titleBook={firstBook.titleBook} author={firstBook.author}>
-        <p>
-          It’s Halloween! What will little Pookie decide to be this year? Pookie tries on costumes one by one, but
-          somehow can’t find just the right thing. The resolution to Pookie’s dilemma will delight toddlers and their
-          caregivers alike
-        </p>
-      </Book>
-
-      <Book img={secondBook.img} titleBook={secondBook.titleBook} author={secondBook.author} />
+      {/* mapping over the books array */}
+      {books.map(book => {
+        //console.log(book)
+        // const { img, titleBook, author } = book
+        return <Book key={book.id} book={book} />
+      })}
     </section>
   )
 }
-const Book = ({ img, titleBook, author, children }) => {
+const Book = props => {
+  //console.log(props)
+  const { img, titleBook, author } = props.book
   return (
-    <article class='book'>
-      <>
-        <img src={img} alt='' />
-        <h1>{titleBook}</h1>
-        <small>{author.toUpperCase()}</small>
-        {children}
-      </>
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{titleBook}</h1>
+      <small>{author.toUpperCase()}</small>
     </article>
   )
 }
