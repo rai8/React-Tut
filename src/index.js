b121dcd59653f44ab2d6dcbb910b231f4e2555a3
rai8 <hytonne@outlook.com>
2020-10-11 17:41:12 +0300

    cart layout and css

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
 const root = document.getElementById('root')
-const Greeting = () => {
+const BookLIst = () => {
   return (
-    <>
-      <Person />
-      <Message />
-    </>
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
-
-const Person = () => {
-  return <h1>Hello there</h1>
+const Book = () => {
+  return (
+    <article class='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
 }
-const Message = () => {
-  return <p>This is a message from John Doe</p>
+const Image = () => {
+  return <img src='https://images-na.ssl-images-amazon.com/images/I/51HU5AkfapL._AC_SX184_.jpg' alt='' />
 }
+const Title = () => {
+  return <h1>Spooky Pookie (Little Pookie)</h1>
+}
+const Author = () => <small style={{ color: '#617d9b', fontWeight: 700, marginTop: '0.5rem' }}>Sandra Boynton </small>
 
-ReactDOM.render(<Greeting />, root)
+ReactDOM.render(<BookLIst />, root)
